53ef5e5c87440ed2f06b5a89eec2eb8c5b1d6dfc
agatapopek <eaa24agpo@students.eaaa.dk>
2025-02-05 23:54:10 +0100

    chat gpt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,119 +1,56 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button';
+import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import profileImage from '../Aplogo.png';
 
-// Stałe
-const drawerWidth = 240;
 const navItems = ['My work', 'About me', 'Contact'];
 
-// Komponent: DrawerContent
-const DrawerContent = ({ handleDrawerToggle }) => (
-  <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-    <img src={profileImage} alt="Profile" className="profile-image" />
-    <Divider />
-    <List>
-      {navItems.map((item) => (
-        <ListItem key={item} disablePadding>
-          <ListItemButton sx={{ textAlign: 'center' }}>
-            <ListItemText primary={item} />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
-  </Box>
-);
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
 
-// Komponent: DrawerAppBar
-const DrawerAppBar = (props) => {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  // Funkcja toggle dla Drawer
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
+  const toggleDrawer = () => {
+    setOpen(!open);
   };
-
-  // Zmienna dla container
-  const container = window !== undefined ? () => window().document.body : undefined;
-
-  // Komponent AppBar
-  const appBar = (
-    <AppBar component="nav" style={{ backgroundColor: '#edb832' }}>
-      <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
-          onClick={handleDrawerToggle}
-          sx={{ mr: 2, display: { sm: 'none' } }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography component="div" sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
-          <img src={profileImage} alt="Profile" style={{ width: '70px', height: 'auto' }} />
-        </Typography>
-        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-          {navItems.map((item) => (
-            <Button key={item} sx={{ color: '#fff' }}>
-              {item}
-            </Button>
-          ))}
-        </Box>
-      </Toolbar>
-    </AppBar>
-  );
-
-  // Komponent Drawer
-  const drawer = (
-    <Drawer
-      container={container}
-      variant="temporary"
-      open={mobileOpen}
-      onClose={handleDrawerToggle}
-      ModalProps={{
-        keepMounted: true, // Better open performance on mobile.
-      }}
-      sx={{
-        display: { xs: 'block', sm: 'none' },
-        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-      }}
-    >
-      <DrawerContent handleDrawerToggle={handleDrawerToggle} />
-    </Drawer>
-  );
 
   return (
     <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      {appBar}
-      <nav>{drawer}</nav>
-      <Box component="main" sx={{ p: 3 }}>
+      <AppBar position="fixed" style={{ backgroundColor: '#edb832' }}>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" onClick={toggleDrawer} sx={{ display: { sm: 'none' } }}>
+            <MenuIcon />
+          </IconButton>
+          <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: { xs: 'center', sm: 'flex-start' } }}>
+            <img src={profileImage} alt="Profile" style={{ width: '70px' }} />
+          </Box>
+          <Box sx={{ marginLeft: 'auto', display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} color="inherit">{item}</Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={open} onClose={toggleDrawer} sx={{ display: { xs: 'block', sm: 'none' } }}>
+        <List>
+          {navItems.map((item) => (
+            <ListItem key={item} onClick={toggleDrawer}>
+              <ListItemText primary={item} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Toolbar />
-        <Typography>
-          {/* Treść strony */}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde fugit veniam eius, perspiciatis sunt?...
-        </Typography>
       </Box>
     </Box>
   );
 };
 
-DrawerAppBar.propTypes = {
-  window: PropTypes.func,
-};
-
-export default DrawerAppBar;
+export default Navbar;
